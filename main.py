baecb195565c232b2e53a168a365edc90819208f
Dmitriy11223455 <vbarishnik50@gmail.com>
2026-01-02 11:04:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import re
 # 1. Сюда вставляйте ваши актуальные ссылки
 urls = [
     "https://edge02r.mediavitrina.ru/hls-livef1/russia1/tracks-v4a1/mono.m3u8?token=v2.qjZuGE7Y62PeX7haw_PAdrwXcQVIOliYBfhz5cN4Ikc.hcXF89oTKmbhgEPYC8bCoPfubOrLAgDsxOgMAeaSnYc.1767948440.97d652f9032c079347440e41d2b6ffee",
+    "https://edge01d.mediavitrina.ru/hls-livef1/1tv/tracks-v4a1/mono.m3u8?token=v2.0V-T0tmqbM24Ve9-rv6GvTGDzmx1oCMt_fP5uiDbxkk._p_zn6pFb91A-5SHMu34GSonOYgnYjFp-aMgHknIA2w.1767949177.2951838d1968d281d63c91b0533cfbb3",
+    "https://edge01p.mediavitrina.ru/hls-livef1/ren-tv/tracks-v4a1/mono.m3u8?token=v2.GLLg0Q0WL99hlrs6Ef4P2aOMOmWyKS6DBnRPiPyUVFc.prA0Nc3_hc36i7INIAyF6oT6TeRf7v-SZf1WWA_g-Dk.1767949284.b8030b2c41841ac38b61dbeeb3d6470f",
+    "https://edge02d.mediavitrina.ru/hls-livef1/ntv0h/tracks-v4a1/mono.m3u8?token=v2.aorh991lIRcxwungV8m7gkqEVJNqxKfzJvXLZc2s_ug.BSEqWZ9p4TQmksxotFTEvoSK-XobY-tz3CGWDEfUW6U.1767949323.d713579ac740958529313228593a0626",
+    "https://edge01r.mediavitrina.ru/hls-livef8/tnt/tracks-v4a1/mono.m3u8?token=v2.cme8xs_V30PNssrBBzdkZb9Fv_MFzDlt8KaUjMn_RIA.CCz5V5e53L_mvGgJHMTqH9wltED-XmJxtYWdMw1O_Fs.1767949374.d48c89913c81e8a565a43e72950c4e76",
+    "https://edge01d.mediavitrina.ru/hls-livef8/zvezda0/tracks-v4a1/mono.m3u8?token=v2.GAV5MF6CeV8ebN1gc5EUxCTDmhsJBz9khxpWfQXToK0.1Hyd0fikPonZT5vsEti1i7fQyy3wDYJe0H3beLljMd0.1767949400.06e140671c70939cea17c4fcc8572edc",
+    "https://edge01r.mediavitrina.ru/hls-livef6/russia24/tracks-v4a1/mono.m3u8?token=v2.72zaSmGxds5G8-sQmS8oNkrfS0yVjTUDkl2cZ11LjdU.6BNbUOy2LWIM6jVXoeeOcHV5XAgr8VxCaOesJ0i0ssA.1767949447.9b05320998c60628792a448db8383060"
     "raw.githubusercontent.com",
     "https://raw.githubusercontent.com/Dmitriy11223455/iptv-autoupdate/refs/heads/main/iptv.m3u"
 ]
